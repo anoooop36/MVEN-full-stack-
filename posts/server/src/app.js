454f4f8fbd9d54288dda37730eb6856c3d121998
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-12-15 21:19:39 +0530

    added mangoose and apis to perform operations on Mongodb using postman

--- posts/server/src/app.js
+++ posts/server/src/app.js
@@ -9,13 +9,46 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
+//connect to mongodb using orm mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error"));
+db.once("open", function(callback){
+    console.log("Connection Succeeded");
+})
+var Post = require("../models/post");
+
+
+
+//write api
 app.get('/posts', (req, res) => {
-    res.send(
-        [{
-            title: "Hello World",
-            description: "Hi there! How are you?"
-        }]
-    )
+    Post.find({'title': 'second post'}, 'title description', function (error, posts) {
+        if (error) { console.error(error); }
+        res.send({
+          posts: posts
+        })
+      })
+})
+
+app.post('/posts', (req, res) => {
+    // var db = req.db;
+    var title = req.body.title;
+    var description = req.body.description;
+    var new_post = new Post({
+      title: title,
+      description: description
+    })
+  
+    new_post.save(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      res.send({
+        success: true,
+        message: 'Post saved successfully!'
+      })
+    })
 })
 
 app.listen(process.env.PORT || 8081)
