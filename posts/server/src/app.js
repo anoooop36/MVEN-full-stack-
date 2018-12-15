e6c4aa9b948472a47fe2a98b22fc0ad2a0a1ccde
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-12-15 21:34:48 +0530

    Added UI to display posts and Add Posts, Fixed issue of fetching all posts

--- posts/server/src/app.js
+++ posts/server/src/app.js
@@ -10,6 +10,7 @@ app.use(cors())
 
 
 //connect to mongodb using orm mongoose
+//create db in mangodb by name posts
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/posts');
 var db = mongoose.connection;
@@ -23,7 +24,7 @@ var Post = require("../models/post");
 
 //write api
 app.get('/posts', (req, res) => {
-    Post.find({'title': 'second post'}, 'title description', function (error, posts) {
+    Post.find({}, 'title description', function (error, posts) {
         if (error) { console.error(error); }
         res.send({
           posts: posts
